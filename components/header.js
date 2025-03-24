bbe3624ff2873295e4fbeb63f06308d9742de497
doyeondev <qazopl123@naver.com>
2025-03-24 21:16:36 +0900

    fix: 자정 이후 버스 시간표 표시 오류 수정
    - 자정 이후 시간대 버스 시간표 표시 버그 수정
    - 남은 버스가 2대 이하일 때 다음날 버스 시간 표시 로직 추가

--- components/header.js
+++ components/header.js
@@ -9,9 +9,6 @@ export default function Header() {
   useEffect(() => {
     async function getPageData() {
       localStorage.theme = 'light'
-      // tippy('#loginBtn', {
-      //   content: '엑세스를 제공 받으신 분만 사용 가능합니다.',
-      // })
     }
     getPageData()
   }, [])
@@ -20,26 +17,9 @@ export default function Header() {
       <header className="body-font fixed h-[60px] w-full bg-transparent bg-transparent px-4 py-2 text-gray-600 sm:px-10">
         <div className="mx-auto flex h-full flex-wrap items-center md:flex-row">
           {/* 홈 링크 */}
-          <div className="flex flex-wrap place-content-center items-center gap-2">
-            {/* <Link href="/" className="title-font flex font-medium text-gray-900">
-              <Image alt="용산03" src="/icon/yongsan_03.svg" width={0} height={0} sizes="100vw" className="w-[240px] justify-center" />
-            </Link> */}
-            {/* <p className="ml-0 text-xl font-bold text-white sm:ml-4 sm:text-3xl">용산03 시간표</p> */}
-          </div>
+          <div className="flex flex-wrap place-content-center items-center gap-2"></div>
           <nav className="ml-auto flex flex-wrap items-center justify-center text-base">
-            {/* <Link href="/" className="mr-5 hover:text-gray-900">
-              홈
-            </Link>
-            <Link href="/projects" className="mr-5 hover:text-gray-900">
-              계약서 작성하기
-            </Link> */}
             <div className="ml-auto flex gap-x-5">
-              {/* <Link href="/login" className="hover:text-gray-900">
-                <button id="loginBtn" className="btn-plain rounded-lg border px-4 py-1">
-                  로그인
-                </button>
-              </Link> */}
-
               <DarkModeToggleButton />
             </div>
           </nav>
